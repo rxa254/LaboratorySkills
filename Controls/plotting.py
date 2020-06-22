f0c36106517f3b7aa82c3a0bfac79962a441d242
Rana Adhikari <rana.adhikari@ligo.org>
2020-06-22 00:55:01 -0700

    updating SISO control example

---
+++ Controls/plotting.py
@@ -0,0 +1,36 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from cycler import cycler
+
+import numpy as np
+
+def set_plot_params():
+    plt.rcParams.update({'text.usetex': False,
+                     'lines.linewidth': 4,
+                     'font.family': 'serif',
+                     'font.serif': 'Georgia',
+                     'font.size': 22,
+                     'xtick.direction': 'in',
+                     'ytick.direction': 'in',
+                     'xtick.labelsize': 'medium',
+                     'ytick.labelsize': 'medium',
+                     'axes.labelsize': 'medium',
+                     'axes.titlesize': 'medium',
+                     'axes.grid.axis': 'both',
+                     'axes.grid.which': 'both',
+                     'axes.grid': True,
+                     'grid.color': 'xkcd:beige',
+                     'grid.alpha': 0.253,
+                     'lines.markersize': 12,
+                     'legend.borderpad': 0.2,
+                     'legend.fancybox': True,
+                     'legend.fontsize': 'small',
+                     'legend.framealpha': 0.8,
+                     'legend.handletextpad': 0.5,
+                     'legend.labelspacing': 0.33,
+                     'legend.loc': 'best',
+                     'figure.figsize': ((12, 8)),
+                     'savefig.dpi': 140,
+                     'savefig.bbox': 'tight',
+                     'pdf.compression': 9})
